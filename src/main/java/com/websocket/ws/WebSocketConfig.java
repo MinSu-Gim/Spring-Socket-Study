593967023c80ed7648eb0eb3c3dec2f9a3f165f4
MinSu-Gim <MinSu_Kim@outlook.kr>
2022-05-27 02:24:03 +0900

    WebSocketConfig íig Class에서 불필요한 코드 지움

--- src/main/java/com/websocket/ws/WebSocketConfig.java
+++ src/main/java/com/websocket/ws/WebSocketConfig.java
@@ -2,14 +2,8 @@ package com.websocket.ws;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.messaging.simp.stomp.StompCommand;
-import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
-import org.springframework.messaging.support.ChannelInterceptor;
-import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -22,8 +16,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(final MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic");
-        registry.setApplicationDestinationPrefixes("/ws", "/user");
+        registry.setApplicationDestinationPrefixes("/ws");
     }
+
     @Override
     public void registerStompEndpoints(final StompEndpointRegistry registry) {
         registry
@@ -32,6 +27,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                 .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
+
     @Override
     public void configureClientInboundChannel(ChannelRegistration registration) {
         registration.interceptors(new UserInterceptor());
